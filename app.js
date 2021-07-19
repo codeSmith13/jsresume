a7af66042f89d43a85133808c4884283eb516a96
codeSmith13 <peeperz13@gmail.com>
2021-07-19 15:54:48 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let myInterest=('cars' , '3D printing' , 'video games' , 'sharks');
+console.log( 
+    'My Intrests:'
++ ('\n') + 
+'* cars'
+ + ('\n') + 
+'* 3D Printing'
++ ('\n') +
+'* video games'
+ + ('\n') + 
+ "* sharks");
+
+console.log(
+    "My previous Expeience: "
+    + ('\n') +
+    "* Odit cum deleniti vitae quibusdam dolorem beatae"
+    + ('\n') +
+    "* Odit cum deleniti vitae quibusdam dolorem beatae"
+    + ('\n') +
+    "* Odit cum deleniti vitae quibusdam dolorem beatae"
+    
+
+
+);
+
+Let mySkills=()
